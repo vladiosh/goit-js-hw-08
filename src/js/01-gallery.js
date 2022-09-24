82d4d56500554aea0cccc3fea9e20f6d10195661
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-24 17:01:02 +0300

    create task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+const imageMarkup = createImageMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createImageMarkup(items) {
+  return items
+    .map(item => {
+      return `
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+  `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
