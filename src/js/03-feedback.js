e41e810443424d483e032602a7a68f4981a6d3b0
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-25 19:31:52 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,10 +6,15 @@ initPage();
 
 const onFormInput = evt => {
   const { name, value } = evt.target;
-  formData[name] = value;
-
   try {
-    const stringifyData = JSON.stringify(formData);
+    let saveData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (saveData) {
+      saveData = JSON.parse(saveData);
+    } else {
+      saveData = {};
+    }
+    saveData[name] = value;
+    const stringifyData = JSON.stringify(saveData);
     localStorage.setItem(LOCAL_STORAGE_KEY, stringifyData);
   } catch (error) {
     console.log(error);
@@ -25,9 +30,8 @@ function initPage() {
   }
   try {
     const parceData = JSON.parse(saveData);
-
-    Object.entries(parceData).forEach(item => {
-      console.log(item);
+    Object.entries(parceData).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
     });
   } catch (error) {
     console.log(error);
